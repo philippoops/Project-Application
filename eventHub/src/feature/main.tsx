4e12bd6935c4c7209edd9845f5445654343cb58b
Philip <philip.buenaflor@gmail.com>
2024-01-11 11:10:19 -0500

    End of Section 1

---
+++ eventHub/src/feature/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../apps/layouts/App.tsx';
+import './apps/layouts/style.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
