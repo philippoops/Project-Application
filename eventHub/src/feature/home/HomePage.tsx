3c7ae24683db71d67d09c79f948050a9501f8d47
Jearmaleen <jearmaleenbautista@yahoo.com>
2024-04-10 20:36:49 -0400

    fixed the homepage look Part 2

--- eventHub/src/feature/home/HomePage.tsx
+++ eventHub/src/feature/home/HomePage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   Button,
   Container,
@@ -7,23 +7,40 @@ import {
   Icon,
   Image,
   Segment,
-} from 'semantic-ui-react';
+} from "semantic-ui-react";
+
+import "./Homepage.css";
 
 export default function HomePage() {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container>
-        <Header as="h1" inverted>
+        <video autoPlay loop muted playsInline className="back-video">
+          <source src="./src/assets/bgimage.mp4" type="video/mp4" />
+        </video>
+
+        <Header as="h1" className="content">
           <Image
             size="massive"
-            src="./logo.png"
+            src="./src/assets/logo.png"
             alt="logo"
-            style={{ marginBottom: 12 }}
+            style={{ marginBottom: 12, width: 400, height: 400 }}
           />
         </Header>
-        <Button size="huge" inverted as={Link} to="/events">
+
+        <Button
+          size="huge"
+          inverted
+          as={Link}
+          to="/events"
+          className="custom-button"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
           Get started
-          <Icon name="caret right" inverted />
+          <Icon name="caret right" />
         </Button>
       </Container>
     </Segment>
